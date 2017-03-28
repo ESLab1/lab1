107d09f2ddc618999a74615a29db83869e7afdb3
alpacaderekyang <derekyang0211@hotmail.com>
2017-03-28 16:38:23 +0800

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -164,6 +164,6 @@ io.on('connection', function(socket) {
 
 }); //end io
 
-http.listen(3000, function(){
+http.listen( (process.env.PORT || 3000), function(){
   console.log('listening on *:3000');
 });
